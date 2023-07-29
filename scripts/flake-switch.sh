248cd93f30272d7534f20f710108dea2e1dc8cca
Andrew Shebanow <ashebanow@gmail.com>
2023-07-29 14:59:26 -0700

    fix echo statements in flake-switch.sh

--- scripts/flake-switch.sh
+++ scripts/flake-switch.sh
@@ -3,12 +3,12 @@
 nix flake update
 
 if [ "$(uname -s)" == "Darwin" ]; then
-  echo "Running 'darwin-rebuild switch --flake '..."
+  echo "Running 'darwin-rebuild switch --flake .'..."
   darwin-rebuild switch --flake .
 elif [ -f "/etc/NIXOS" ]; then
-  echo "Running 'sudo nixos-rebuild switch --flake '..."
+  echo "Running 'sudo nixos-rebuild switch --flake .'..."
   sudo nixos-rebuild switch --flake .
 else
-  echo "Running 'home-manager switch --impure --flake '..."
+  echo "Running 'home-manager switch --impure --flake .'..."
   home-manager switch --impure --flake .
 fi
