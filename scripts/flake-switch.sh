95a28533ee509f6a4446ac6b4f77bdffdc02c7ec
Andrew Shebanow <ashebanow@gmail.com>
2023-08-24 13:15:49 -0700

    add tracing in flake-switch script

--- scripts/flake-switch.sh
+++ scripts/flake-switch.sh
@@ -4,11 +4,11 @@ nix flake update
 
 if [ "$(uname -s)" == "Darwin" ]; then
   echo "Running 'darwin-rebuild switch --flake .'..."
-  darwin-rebuild switch --flake .
+  darwin-rebuild switch --show-trace --flake .
 elif [ -f "/etc/NIXOS" ]; then
   echo "Running 'sudo nixos-rebuild switch --flake .'..."
-  sudo nixos-rebuild switch --flake .
+  sudo nixos-rebuild switch --show-trace --flake .
 else
   echo "Running 'home-manager switch --impure --flake .'..."
-  home-manager switch --impure --flake .
+  home-manager switch --show-trace --impure --flake .
 fi
