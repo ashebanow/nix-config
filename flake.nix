c91531906d6b58fed2f37e56ea62a6188f9b728d
Andrew Shebanow <ashebanow@gmail.com>
2023-09-07 15:52:08 -0700

    rename configuration.nix to be machine specific

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     home-manager = {
-      url = "github:nix-community/home-manager/archive/release-23.05.tar.gz";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     darwin = {
@@ -32,7 +32,16 @@
           packages = linuxSystem;
           specialArgs = inputs; # forward inputs to modules
           modules = [
-            ./system/nixos/configuration.Nix
+            ./system/nixos/virt1-configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.ashebanow = import ./home/home.nix;
+
+              # Optionally, use home-manager.extraSpecialArgs to pass
+              # arguments to home.nix
+            }
           ];
         };
       };
