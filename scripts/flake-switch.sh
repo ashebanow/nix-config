6674e585073e9120d8c9d6a353334d319b132fd0
Andrew Shebanow <ashebanow@gmail.com>
2023-09-09 10:41:58 -0700

    tweak args in shell script

--- scripts/flake-switch.sh
+++ scripts/flake-switch.sh
@@ -6,9 +6,9 @@ if [ "$(uname -s)" == "Darwin" ]; then
   echo "Running 'darwin-rebuild switch --flake .'..."
   darwin-rebuild switch --show-trace --flake .
 elif [ -f "/etc/NIXOS" ]; then
-  echo "Running 'sudo nixos-rebuild switch --flake .'..."
-  sudo nixos-rebuild switch --show-trace --flake .
+  echo "Running 'sudo nixos-rebuild --impure switch --flake .'..."
+  sudo nixos-rebuild switch --impure --flake .
 else
   echo "Running 'home-manager switch --impure --flake .'..."
-  home-manager switch --show-trace --impure --flake .
+  home-manager switch --impure --flake .
 fi
