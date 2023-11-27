f4928661b1f5f2ca80d007f8e482892f3ad0baba
Andrew Shebanow <ashebanow@gmail.com>
2023-11-27 14:33:57 -0800

    Add terraform/vault/etc to config

---
+++ home/common/hashi.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, ... }: {
+
+  home.packages = with pkgs; [
+    consul
+    # damon
+    levant
+    # nomad
+    # nomad-autoscaler
+    # nomad-pack
+    packer
+    terraform
+    terragrunt
+    tflint
+    vault
+  ];
+}
