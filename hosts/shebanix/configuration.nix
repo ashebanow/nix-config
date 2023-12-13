0aa951d09999eac0123409569ab2d9ba015eee3a
Andrew Shebanow <ashebanow@gmail.com>
2023-12-13 10:14:30 -0800

    remove k3s from shebanix

--- hosts/shebanix/configuration.nix
+++ hosts/shebanix/configuration.nix
@@ -37,24 +37,12 @@
         22              # ssh
         80 443          # HTTP/HTTPS
         2049            # nfs
-        6443            # k3s: required so that pods can reach the API server (running on port 6443 by default)
-        # 2379          # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
-        # 2380          # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
-      ];
-      allowedUDPPorts = [
-        # 8472          # k3s, flannel: required if using multi-node for inter-node networking
       ];
     };
   };
 
   # Enable network manager applet
   programs.nm-applet.enable = true;
-
-  services.k3s.enable = true;
-  services.k3s.role = "server";
-  services.k3s.extraFlags = toString [
-    # "--kubelet-arg=v=4" # Optionally add additional args to k3s
-  ];
 
   services.openssh = {
     enable = true;
@@ -239,7 +227,6 @@
     chromium
     cifs-utils
     docker-compose
-    k3s  
     kitty
     nfs-utils
     samba
