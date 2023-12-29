de8bdd8a1cfb87a2bc9e71a722544ff9040dc25d
Andrew Shebanow <ashebanow@gmail.com>
2023-12-29 13:07:51 -0800

    Add remaining samba shares to loquat, and copy to virt1/virt2

--- hosts/loquat/configuration.nix
+++ hosts/loquat/configuration.nix
@@ -65,6 +65,7 @@
 
     firewall = {
       enable = true;
+      allowPing = true;
       allowedTCPPorts = [ 
         22              # ssh
         80 443          # HTTP/HTTPS
@@ -88,8 +89,14 @@
     enable = true;
     port = 9090;
     openFirewall = false;
+    settings = {
+      WebService = {
+        AllowUnencrypted = true;
+      };
+    };
   };
 
+  # Enable docker
   virtualisation.docker = {
     enable = true;
     enableOnBoot = true;
@@ -102,66 +109,112 @@
   # Mount SMB shares from storage machine. All of these are set to automount
   # on first use, and unmount after 10 minutes
   #
+  # see exmaple from https://fictionbecomesfact.com/nixos-server-configuration
+  #
   # NOTE THAT YOU MUST CREATE/COPY the '/etc/nixos/smb-secrets' file to the
   # machine, with USERNAME, DOMAIN and PASSWORD defined on separate lines.
   # FIXME: replace with vault secrets
-  # services.rpcbind.enable = true;
+  services.rpcbind.enable = true;
+  services.gvfs.enable = true;
   # services.nfs.server.enable = true;
-  # services.gvfs.enable = true;
   # boot.initrd = {
   #   supportedFilesystems = [ "nfs" ];
   #   kernelModules = [ "nfs" ];
   # };
-  # fileSystems = {
-  #   "/mnt/users/appdata" = {
-  #     device = "//storage/mnt/users/appdata";
-  #     fsType = "cifs";
-  #     options = [
-  #       "x-systemd.automount"
-  #       "noauto"
-  #       "credentials=/etc/nixos/smb-secrets"
-  #       "x-systemd.idle-timeout=60"
-  #       "x-systemd.device-timeout=5s"
-  #       "x-systemd.mount-timeout=5s"
-  #     ];
-  #   };
-    # "/mnt/users/backups" = {
-    #   device = "//storage/mnt/users/backups";
-    #   fsType = "cifs";
-    #   options = [
-    #     "x-systemd.automount"
-    #     "noauto"
-    #     "credentials=/etc/nixos/smb-secrets"
-    #     "x-systemd.idle-timeout=60"
-    #     "x-systemd.device-timeout=5s"
-    #     "x-systemd.mount-timeout=5s"
-    #   ];
-    # };
-    # "/mnt/users/isos" = {
-    #   device = "//storage/mnt/users/isos";
-    #   fsType = "cifs";
-    #   options = [
-    #     "x-systemd.automount"
-    #     "noauto"
-    #     "credentials=/etc/nixos/smb-secrets"
-    #     "x-systemd.idle-timeout=60"
-    #     "x-systemd.device-timeout=5s"
-    #     "x-systemd.mount-timeout=5s"
-    #   ];
-    # };
-    # "/mnt/users/media" = {
-    #   device = "//storage/mnt/users/media";
-    #   fsType = "cifs";
-    #   options = [
-    #     "x-systemd.automount"
-    #     "noauto"
-    #     "credentials=/etc/nixos/smb-secrets"
-    #     "x-systemd.idle-timeout=60"
-    #     "x-systemd.device-timeout=5s"
-    #     "x-systemd.mount-timeout=5s"
-    #   ];
-    # };
-  # };    
+  fileSystems = {
+    "/mnt/storage/appdata" = {
+      device = "//storage.lan/appdata";
+      fsType = "cifs";
+      options = [
+        # Prevent hanging on network loss
+        "x-systemd.automount"+
+        "noauto"
+        "x-systemd.idle-timeout=60"
+        "x-systemd.device-timeout=5s"
+        "x-systemd.mount-timeout=5s"
+
+        # Specify location of credentials file
+        "credentials=/etc/nixos/smb-secrets"
+
+        # Default permissions and file encoding
+        "file_mode=0777"
+        "dir_mode=0777"
+        "iocharset=utf8"
+      ];
+    };
+    "/mnt/storage/backups" = {
+      device = "//storage.lan/backups";
+      fsType = "cifs";
+      options = [
+        # Prevent hanging on network loss
+        "x-systemd.automount"
+        "noauto"
+        "x-systemd.idle-timeout=60"
+        "x-systemd.device-timeout=5s"
+        "x-systemd.mount-timeout=5s"
+
+        # Specify location of credentials file
+        "credentials=/etc/nixos/smb-secrets-ashebanow"
+
+        # Default permissions and file encoding
+        "file_mode=0777"
+        "dir_mode=0777"
+        "iocharset=utf8"
+
+        # mount as user, not root
+        "uid=1000"
+        "gid=1000"
+      ];
+    };
+    "/mnt/storage/isos" = {
+      device = "//storage.lan/isos";
+      fsType = "cifs";
+      options = [
+        # Prevent hanging on network loss
+        "x-systemd.automount"+
+        "noauto"
+        "x-systemd.idle-timeout=60"
+        "x-systemd.device-timeout=5s"
+        "x-systemd.mount-timeout=5s"
+
+        # Specify location of credentials file
+        "credentials=/etc/nixos/smb-secrets-ashebanow"
+
+        # Default permissions and file encoding
+        "file_mode=0777"
+        "dir_mode=0777"
+        "iocharset=utf8"
+
+        # mount as user, not root
+        "uid=1000"
+        "gid=1000"
+      ];
+    };
+    "/mnt/storage/media" = {
+      device = "//storage.lan/media";
+      fsType = "cifs";
+      options = [
+        # Prevent hanging on network loss
+        "x-systemd.automount"+
+        "noauto"
+        "x-systemd.idle-timeout=60"
+        "x-systemd.device-timeout=5s"
+        "x-systemd.mount-timeout=5s"
+
+        # Specify location of credentials file
+        "credentials=/etc/nixos/smb-secrets-ashebanow"
+
+        # Default permissions and file encoding
+        "file_mode=0777"
+        "dir_mode=0777"
+        "iocharset=utf8"
+
+        # mount as user, not root
+        "uid=1000"
+        "gid=1000"
+      ];
+    };
+  };    
 
   # Set your time zone.
   time.timeZone = "America/Los_Angeles";
@@ -317,7 +370,7 @@
     polkitPolicyOwners = [ "ashebanow" ];
   };
 
-  # List services that you want to enable:
+  # Enable QMEU emulation
   services.qemuGuest.enable = true;
   virtualisation.libvirtd.enable = true;
   programs.dconf.enable = true;
