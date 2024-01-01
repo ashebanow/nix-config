1f002b260a3842cdd952e8aedad4b155a7aa5933
Andrew Shebanow <ashebanow@gmail.com>
2024-01-01 15:56:50 -0800

    Add some basic apps to systemPackages on liquid-nixos

--- hosts/liquid-nixos/configuration.nix
+++ hosts/liquid-nixos/configuration.nix
@@ -38,6 +38,16 @@
   wsl.defaultUser = "ashebanow";
   wsl.wslConf.network.generateHosts = false;
 
+  networking.hostName = "liquid-nixos";
+
+  environment.systemPackages = with pkgs; [
+    firefox
+    git
+    just
+    kitty
+    vim
+  ];
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
