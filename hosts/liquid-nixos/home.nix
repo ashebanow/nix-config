e01da074d6ba8f5e27a8d68944adf4c38695949e
Andrew Shebanow <ashebanow@gmail.com>
2024-01-02 04:30:39 +0000

    Made a liquid-nixos specific home.nix file

---
+++ hosts/liquid-nixos/home.nix
@@ -0,0 +1,39 @@
+{ nixpkgs, config, ... }:
+
+{
+  nixpkgs.config.allowUnfree = true;
+
+  imports = [
+    ../../modules/core.nix
+    ../../modules/chrome.nix
+    ../../modules/git.nix
+    ../../modules/hashi.nix
+    ../../modules/iputils.nix
+    # ../../modules/lorri.nix
+    ../../modules/neovim.nix
+    ../../modules/shell.nix
+    ../../modules/warp.nix
+    ../../modules/vscode.nix
+
+    # "${fetchTarball "https://github.com/msteen/nixos-vscode-server/tarball/master"}/modules/vscode-server/home.nix"
+
+    # <sops-nix/modules/home-manager/sops.nix>
+  ];
+
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "ashebanow";
+
+  home.homeDirectory = "/home/ashebanow";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "23.11"; # Please read the comment before changing.
+
+  programs.home-manager.enable = true;
+}
