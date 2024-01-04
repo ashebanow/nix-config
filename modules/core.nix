c5de759f4def1b014db4449c565b9c7ad8453b28
Andrew Shebanow <ashebanow@gmail.com>
2024-01-04 00:35:27 +0000

    Add a test-starship script

--- modules/core.nix
+++ modules/core.nix
@@ -7,6 +7,7 @@
     ".local/bin/find-dirty-gits".source = ../scripts/find-dirty-gits;
     ".local/bin/list-cloudflare-ips".source = ../scripts/list-cloudflare-ips;
     ".local/bin/make-dev-shell".source = ../scripts/make-dev-shell;
+    ".local/bin/test-starship".source = ../scripts/test-starship;
     ".config/glances/glances.conf".source = ../dotfiles/glances.conf;
     ".config/neofetch/config".source = ../dotfiles/neofetch-config;
     ".config/starship.toml".source = ../dotfiles/starship.toml;
