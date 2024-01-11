17cc23336f525f99f4cc896ebc3d00f161fabb17
Andrew Shebanow <ashebanow@gmail.com>
2024-01-11 14:06:54 -0800

    Prep for incoming yuzu system, and tweak loquat's IP address

--- hosts/virt2/configuration.nix
+++ hosts/virt2/configuration.nix
@@ -49,10 +49,11 @@
 
     extraHosts = ''
       10.40.0.1   gateway gateway.lan
-      10.40.0.226  storage storage.lan
+      10.40.0.227  storage storage.lan
       10.40.0.11  virt1 virt1.lan
       10.40.0.14  virt2 virt2.lan
-      10.40.0.118 loquat loquat.lan
+      10.40.60.6 yuzu yuzu.lan
+      10.40.60.7 loquat loquat.lan
       172.21.75.8 liquid-nixos liquid-nixos.lan
     '';
 
