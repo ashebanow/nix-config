54b94cc24027229e447680525cea7d6f267267a5
Andrew Shebanow <ashebanow@gmail.com>
2024-03-05 15:00:15 -0800

    More hyprland config tweaking

--- modules/nixos/desktops/hyprland.nix
+++ modules/nixos/desktops/hyprland.nix
@@ -3,16 +3,48 @@
   pkgs,
   ...
 }: {
-  # Enable Hyprland
-  programs.hyprland.enable = true;
-  package = inputs.hyprland.packages.${pkgs.system}.hyprland;
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
   environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
 
+  # Enable Hyprland
+  programs.hyprland = {
+    enable = true;
+    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+    xwayland.hidpi = true;
+    xwayland.enable = true;
+  };
+
+  services.dbus.enable = true;
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gtk
+    ];
+  };
+
+  # for xremap to work with wlroots
+  services.xremap.withWlroots = true;
+
   environment.systemPackages = with pkgs; [
+    hyprland
+    dunst
+    libnotify
+    meson
+    pavucontrol
+    rofi-wayland
+    swww # for wallpapers
+    wayland-protocols
+    wayland-utils
+    wl-clipboard
+    wlroots
+    xdg-desktop-portal-gtk
+    xdg-desktop-portal-hyprland
+    xwayland
+
     foot # terminal
-    zathura # pdf viewer
-    mpv # media player
     imv # image viewer
+    mpv # media player
+    zathura # pdf viewer
   ];
 }
