5680fe06e5ad1fb9625210531ff629cb318c9a72
Andrew Shebanow <ashebanow@gmail.com>
2024-03-07 18:28:17 -0800

    Get rid of liquid-nixos system, replaced by limon.

--- flake.nix
+++ flake.nix
@@ -118,20 +118,6 @@
           }
         ];
       };
-      liquid-nixos = nixpkgs.lib.nixosSystem {
-        system = linuxSystem;
-        specialArgs = {inherit inputs;}; # forward inputs to modules
-        modules = [
-          ./hosts/liquid-nixos/configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.verbose = true;
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.ashebanow = import ./hosts/liquid-nixos/home.nix;
-          }
-        ];
-      };
     };
 
     darwinConfigurations = {
