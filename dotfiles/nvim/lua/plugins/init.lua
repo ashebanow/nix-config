1558de3ca7b6707695e42cf95274a2d526a38154
Andrew Shebanow <ashebanow@gmail.com>
2024-03-21 13:01:29 -0700

    WIP on catpuccin for nvim

--- dotfiles/nvim/lua/plugins/init.lua
+++ dotfiles/nvim/lua/plugins/init.lua
@@ -1,4 +1,6 @@
 return {
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+
   {
     "stevearc/conform.nvim",
     config = function()
@@ -12,4 +14,40 @@ return {
       git = { enable = true },
     },
   },
+
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*",  -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    -- event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+    --   "BufReadPre path/to/my-vault/**.md",
+    --   "BufNewFile path/to/my-vault/**.md",
+    -- },
+    dependencies = {
+      -- Required.
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "main",
+          # TODO: edit linux path as needed...
+          path = (vim.loop.os_uname().sysname == "Darwin")
+            ? "~/Documents/Obsidian Vaults/Main"
+            : "~/Documents/Obsidian Vaults/Main",
+        },
+      },
+      -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
+      completion = {
+        -- Set to false to disable completion.
+        nvim_cmp = true,
+        -- Trigger completion at 2 chars.
+        min_chars = 2,
+      },
+    },
+    },
 }
