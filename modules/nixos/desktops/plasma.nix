eff2c53b5cf7f4d056870c35acc8b2ca0b884b14
Andrew Shebanow <ashebanow@gmail.com>
2024-04-22 14:10:54 -0700

    plasma 6 update

--- modules/nixos/desktops/plasma.nix
+++ modules/nixos/desktops/plasma.nix
@@ -3,8 +3,6 @@
   services.xserver = {
     enable = true;
 
-    desktopManager.plasma6.enable = true;
-
     # Configure keymap in X11
     xkb.layout = "us";
     xkb.variant = "";
@@ -12,6 +10,12 @@
   };
 
   # Enable the KDE Plasma Desktop Environment.
+  services.desktopManager = {
+    plasma6 = {
+      enable = true;
+    };
+  };
+
   services.displayManager = {
     sddm = {
       enable = true;
