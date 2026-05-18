913463b7403d5b58cf270fa9447fb7ba9b38cfcb
ashebanow <ashebanow@gmail.com>
2026-05-18 10:25:43 -0700

    WIP on adding git signing, plus a few other cleanups.

--- nixos/base/user.nix
+++ nixos/base/user.nix
@@ -1,9 +1,20 @@
 {
   flake.nixosModules.base = {lib, ...}: {
     options.preferences = {
-      user.name = lib.mkOption {
-        type = lib.types.str;
-        default = "ashebanow";
+      user = {
+        name = lib.mkOption {
+          type = lib.types.str;
+          default = "ashebanow";
+        };
+        email = lib.mkOption {
+          type = lib.types.str;
+          default = "ashebanow@gmail.com";
+        };
+        signingKey = lib.mkOption {
+          type = lib.types.path;
+          default = /home/ashebanow/.ssh/github_ed25519.pub;
+          description = "Path to SSH public key for commit signing";
+        };
       };
     };
   };
--- wrappedPrograms/git.nix
+++ wrappedPrograms/git.nix
@@ -1,11 +1,28 @@
-{inputs, ...}: {
+{
+  inputs,
+  lib,
+  config,
+  ...
+}: let
+  cfg = config.programs.git;
+  userCfg = config.preferences.user;
+in {
+  options.programs.git = {
+    enableCodeSigning = lib.mkEnableOption "SSH-based code signing for commits";
+  };
+
+  config.programs.git.enableCodeSigning = lib.mkDefault false;
+
   perSystem = {pkgs, ...}: {
     packages.git = inputs.wrappers.lib.wrapPackage {
       inherit pkgs;
       package = pkgs.git;
+      # FIX: we need to do more than define env variables.
+      # We also need to write the git config file and possible edit
+      # ssh config.
       env = rec {
-        GIT_AUTHOR_NAME = "Andrew Shebanow";
-        GIT_AUTHOR_EMAIL = "ashebanow@gmail.com";
+        GIT_AUTHOR_NAME = config.users.users.${userCfg.name}.fullName;
+        GIT_AUTHOR_EMAIL = userCfg.email;
         GIT_COMMITTER_NAME = GIT_AUTHOR_NAME;
         GIT_COMMITTER_EMAIL = GIT_AUTHOR_EMAIL;
       };
