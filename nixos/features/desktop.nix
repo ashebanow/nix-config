a79aab10544b344805a9e88e39aea10a0bb511cc
ashebanow <ashebanow@gmail.com>
2026-05-20 09:41:35 -0700

    Fix time zone in desktop.zix

--- nixos/features/desktop.nix
+++ nixos/features/desktop.nix
@@ -35,19 +35,8 @@
       monospace = ["JetBrainsMono Nerd Font"];
     };
 
-    time.timeZone = "Europe/Kyiv";
+    time.timeZone = "America/LosAngeles";
     i18n.defaultLocale = "en_US.UTF-8";
-    i18n.extraLocaleSettings = {
-      LC_ADDRESS = "uk_UA.UTF-8";
-      LC_IDENTIFICATION = "uk_UA.UTF-8";
-      LC_MEASUREMENT = "uk_UA.UTF-8";
-      LC_MONETARY = "uk_UA.UTF-8";
-      LC_NAME = "uk_UA.UTF-8";
-      LC_NUMERIC = "uk_UA.UTF-8";
-      LC_PAPER = "uk_UA.UTF-8";
-      LC_TELEPHONE = "uk_UA.UTF-8";
-      LC_TIME = "uk_UA.UTF-8";
-    };
 
     services.upower.enable = true;
 
