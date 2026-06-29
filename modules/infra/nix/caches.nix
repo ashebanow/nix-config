a2ff7e536bdaf06f55df2096ba062fda526af9c3
Andrew Shebanow <ashebanow@gmail.com>
2026-06-29 11:36:13 -0700

    caches: remove attic.xuyh0120.win binary cache
    lantian's attic cache is hosted in China and times out from lumquat,
    adding ~15s of warnings to every nixos-rebuild. Lumquat doesn't use
    any packages from this cache.

--- modules/infra/nix/caches.nix
+++ modules/infra/nix/caches.nix
@@ -3,7 +3,6 @@ let
     "https://cache.flakehub.com"
     "https://nix-community.cachix.org"
     "https://cache.nixos.org/"
-    "https://attic.xuyh0120.win/lantian"
     "https://cache.numtide.com"
     "https://pi.cachix.org"
   ];
@@ -11,7 +10,6 @@ let
     "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     "hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs="
-    "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
     "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
     "pi.cachix.org-1:lGeoGJaZ5ZDabuRzkcD5EBTNnDM4HJ1vqeOxlWk1Flk="
   ];
